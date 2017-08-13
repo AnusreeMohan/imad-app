528588c787509404739714e918ef74f7d6b768ff
AnusreeMohan <30665659+AnusreeMohan@users.noreply.github.com>
2017-08-13 18:21:40 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element = document.getElementByID('main-text');
 element.innerHTML = "New Value";
 //move the image
-var img = document.getElemetByID('madi');
+var img = document.getElementByID('madi');
 img.onClick = function(){
     img.style.marginLeft = '100px';
 };
