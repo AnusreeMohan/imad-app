2696ce273674e199b9e0c6c770e5f309c69294f8
AnusreeMohan <30665659+AnusreeMohan@users.noreply.github.com>
2017-08-13 18:50:40 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,12 @@ var element = document.getElementById('main-text');
 element.innerHTML = "New Value";
 //move the image
 var img = document.getElementById('madi');
+var moveLeft = 0;
+function moveRight(){
+    moveLeft += 10;
+    img.style.marginLeft = moveLeft +'px';
+}
 img.onclick = function(){
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight, 100);
+   // img.style.marginLeft = '100px';
 };
