7aaa122aa02e2ab8f1b0e37fad8a684b1ca7640c
AnusreeMohan <30665659+AnusreeMohan@users.noreply.github.com>
2017-08-15 18:23:08 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,8 +22,7 @@ button.onclick = function(){
 };
 
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //make a request to the server and send the name
@@ -34,7 +33,8 @@ var request = new XMLHttpRequest();
         if(request.readyState === XMLHttpRequest.DONE){
             //some action
             if(request.status === 200){
-                 var names = ['name1','name2','name3','name4'];
+                 var names = request.responseText;
+                 names = JSON.parse(names);
                     var list ='';
                     for(var i=0; i<names.length; i++){
                         list += '<li>'+names[i]+'</li>';
@@ -45,6 +45,8 @@ var request = new XMLHttpRequest();
         }
     };
     //Make a request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
    request.open('GET', 'http://anusremohan.imad.hasura-app.io/submit-name?name='+name,true);
    request.send(null);
    
